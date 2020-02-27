72c5f406d67818083b3a2c86ce7f01cea58c792d
Aidan Middleton <aidanemiddleton@gmail.com>
2020-02-27 03:36:27 +0000

    added layout for adding tweets, still need to implement the functions, purely framework at this point

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
   const countHandler = function() {
-    //  const tweetEntryBox = $('.tweetEntryBox').siblings().first();
     const tweetEntryBox = $('.tweetEntryBox').siblings().first().children('.counter')
     tweetEntryBox.text((140 - $(this).val().length))
     if ((140 - $(this).val().length )< 0) {
