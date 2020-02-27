f948247747071f05a5f7febfa5122c226123e030
Aidan Middleton <aidanemiddleton@gmail.com>
2020-02-27 02:01:02 +0000

    added styling file for the individual tweet boxes, and styled a hard coded example

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
 
-   const countHandler = function() {
+  const countHandler = function() {
     //  const tweetEntryBox = $('.tweetEntryBox').siblings().first();
-     const tweetEntryBox = $('.tweetEntryBox').siblings().first().children('.counter')
-     console.log(tweetEntryBox);
-     tweetEntryBox.text((140 - $(this).val().length))
+    const tweetEntryBox = $('.tweetEntryBox').siblings().first().children('.counter')
+    tweetEntryBox.text((140 - $(this).val().length))
+    if ((140 - $(this).val().length )< 0) {
+      tweetEntryBox.css({color: 'red'});
+    }
      
    }
   $('.tweetEntryBox').keyup(countHandler);
