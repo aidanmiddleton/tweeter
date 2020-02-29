d1e6294b6b2cf4780bb01b3045965ac27912ab87
Aidan Middleton <aidanemiddleton@gmail.com>
2020-02-29 22:34:55 +0000

    final bug fixes and minor styling changes, code cleanup, etc.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,10 +1,7 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
 $(document).ready(() => {
     
+
+//uses createTweetElement to format the data being recieved from the server, prepends them to the tweet container in chronological order
   const renderTweets = function(tweets) {
     for (let tweet of tweets) {
       let $currentTweet = createTweetElement(tweet);
@@ -12,39 +9,47 @@ $(document).ready(() => {
     }
   };
   
+// V-- creates tweet elements through jquery creation of elements, followed by appending in order to create the articles. Ensures classes will be correct, and that styling will be properly applied. 
+
   const createTweetElement = function(tweet) {
     const $tweet = $('<article>').addClass('tweet');
-    const $header = $('<header>')
+    const $header = $('<header>');
     const $tweetHeader = $('<div>').addClass('tweetHeader');
     const $avatar = $('<img>').addClass('tweetPic').attr('src', tweet.user.avatars);
     const $userName = $('<p>').addClass('usersName').text(tweet.user.name);
     const $usersHandle = $('<p>').addClass('usershandle').text(tweet.user.handle);
     const $tweetContents = $('<p>').addClass('tweetContents').text(tweet.content.text);
     const $tweetFooter = $('<footer>').addClass('tweetFooter').text(`posted ${moment(tweet.created_at).fromNow()}`);
-    const $iconsDiv = $('<div>').addClass('icons')
+    const $iconsDiv = $('<div>').addClass('icons');
     const $retweet = $('<i>').addClass("fas fa-retweet");
     const $heart = $('<i>').addClass("fas fa-heart");
     const $flag = $('<i>').addClass("fas fa-flag");
+
+
     $tweetHeader.append($avatar, $userName, $usersHandle);
     $iconsDiv.append($retweet, $flag, $heart);
     $header.append($tweetHeader);
     $tweetFooter.append($iconsDiv);
     $tweet.append($header, $tweetContents, $tweetFooter);
     return $tweet;
-  }
+  };
+
+//retrieves tweets from server and calls the renderTweets function
 
   const loadTweets = function() {
     $.get('/tweets', function(result) {
       renderTweets(result);
     })
-  }
+  };
 
   loadTweets()
 
   const clearForm = function() {
     $( ".submission-form .tweetEntryBox" ).val(null);
     $( ".submission-form .counter").text(140);
-  }
+  };
+
+// jquery functions to handle the creation and posting of new tweets by users, including auto feed refresh, and validation error handling
 
   $( ".submission-form" ).submit(function( event ) {
     event.preventDefault();
@@ -75,6 +80,7 @@ $(document).ready(() => {
     $('.new-tweet')
     .slideToggle(500)
     .focus();
+    $('.tweetEntryBox').focus()
   })
 
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,14 @@
+// Function that handles the character counter on the tweet entry field
+
 $(document).ready(function() {
 
   const countHandler = function() {
     const tweetEntryBox = $('.tweetEntryBox').siblings().first().children('.counter')
-    tweetEntryBox.text((140 - $(this).val().length))
+    tweetEntryBox.text((140 - $(this).val().length));
     if ((140 - $(this).val().length )< 0) {
       tweetEntryBox.css({color: 'red'});
     } else {
-      tweetEntryBox.css({color: 'rgb(248, 130, 34)'})
+      tweetEntryBox.css({color: 'rgb(248, 130, 34)'});
     }
      
    }
